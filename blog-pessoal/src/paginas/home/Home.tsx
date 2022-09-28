5d2d6485d971eb732bec32d321059a185ce8f192
kelvyn777 <kelvynsabino10@gmail.com>
2022-09-28 12:07:33 -0300

    Implementando componentes

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://c4.wallpaperflare.com/wallpaper/724/91/678/brazil-corinthians-fiel-sinalizador-wallpaper-preview.jpg"
+            alt="Imagem da tela inicial"className="img" />
+        </>
+    )
+}
+
+export default Home;
