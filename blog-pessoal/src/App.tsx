421e2fa15cb8ae3bae71925ae612765b94618d0a
kelvyn777 <kelvynsabino10@gmail.com>
2022-09-30 22:38:14 -0300

    Refatorando estilo dos componentes

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -3,16 +3,26 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './paginas/login/Login';
+
 
 
 
 function App() {
   return (
-          <>
-            <Navbar/>
-              <Home/>
-            <Footer/>  
-          </>
+    <BrowserRouter>
+      <Navbar />
+        <div style={{ minHeight: '100vh' }}>
+          <Routes> {/*Antigo Switch*/}
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+              {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+            </Routes>
+          </div>
+        <Footer />
+    </BrowserRouter >
   );
 }
 
